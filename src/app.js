c94ebdc43a96cd766808109c77f58510a6dfb4ef
tiblazy <hauntedapply@gmail.com>
2022-05-10 11:47:44 -0300

    init blog-M2

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import Modal from "./models/Modal.js"; // chama o form
+// import Form from "./models/Form.js"; // chama o post
+import Post from "./models/Post.js"; // se tiver logado aparece
+
+
+// Modal.cadastrar();
+// Modal.login();
+
+// Form.register();
+// Form.login();
+
+Post.header();
+Post.new();
+Post.message();
